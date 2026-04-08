6f6c5f8d10da434dccc789876c721db0b9a92df7
Rodrigo <sistsayao@gmail.com>
2026-04-08 18:37:26 -0300

    Refactor rental cost calculation with constants

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -7,21 +7,25 @@
 
 function calculateRentalCost(days) {
 let desc = 0;
+
+  const COST_PER_DAY = 40;
+   const DESC_3 = 20;
+   const DESC_7 = 50;
   
 if(days<3){
- desc = days * 40;
+ desc = days * COST_PER_DAY;
 }
 
   
 if(days>=3 && days<=6){
- desc = days * 40 - 20;
+ desc = days * COST_PER_DAY - DESC_3;
 }
 
 
 
 if(days>=7){
 
-  desc = days * 40 - 50;
+  desc = days * COST_PER_DAY - DESC_7;
 
 }
 
